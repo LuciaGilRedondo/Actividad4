4efeb2931ef071b4edd80562742f0f787440eabc
Lucia <lucia.gilred@educa.jcyl.es>
2023-02-17 20:57:55 +0100

    Primer commit de la actividad 4

---
+++ src/actividad4/LG_Actividad4.java
@@ -0,0 +1,13 @@
+package actividad4;
+
+public class LG_Actividad4 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i==5; i++) {
+			System.out.println("Hola");
+		}
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dam1t08
+ *
+ */
+module LG_Actividad4 {
+}
